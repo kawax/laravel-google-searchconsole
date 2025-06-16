a8af186a98488aaa001214f336c7fab2da20df82
kawax <kawaxbiz@gmail.com>
2025-06-16 13:09:51 +0900

    Update console.php

--- workbench/routes/console.php
+++ workbench/routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Revolution\Google\SearchConsole\Facades\SearchConsole;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote');
+
+Artisan::command('sc:test', function () {
+    dump(env('GOOGLE_SERVICE_ACCOUNT_JSON_LOCATION'));
+    dump(config('google.service.file'));
+    dump(SearchConsole::listSites());
+});
